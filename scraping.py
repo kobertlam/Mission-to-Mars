6fc665ed660e5ad71153adcd87d1662315a2dae7
Kobert Lam <kobertlam@gmail.com>
2021-12-27 13:42:36 -0500

    completed 10.5.2

--- scraping.py
+++ scraping.py
@@ -10,64 +10,80 @@ from webdriver_manager.chrome import ChromeDriverManager
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
-# Visit the mars nasa news site
-url = 'https://redplanetscience.com'
-browser.visit(url)
-# Optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+def mar_news(browser):
+    # Visit the mars nasa news site
+    url = 'https://redplanetscience.com'
+    browser.visit(url)
 
-# Using "select_one" function, the first matching element returned will be a \<li /> element with a class of "slide" and all nested elements within it.
-html = browser.html
-news_soup = soup(html, 'html.parser')
-slide_elem = news_soup.select_one('div.list_text')
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-# The specific data is in a <div /> with a class of 'content_title'
-slide_elem.find('div', class_='content_title')
+    # Convert the browser html to a soup object
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
 
-# Use the parent element to find the first `a` tag and save it as `news_title`
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
+    # Add try/except for error handling for scraping
+    try:
+        # Using "select_one" function, the first matching element returned will be a \<li /> element with a class of "slide" and all nested elements within it.
+        slide_elem = news_soup.select_one('div.list_text')
 
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_p
+        # Use the parent element to find the first `a` tag and save it as `news_title`
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+
+    except AttributeError:
+        return None, None
+    # Return the news title and paragraph
+    return news_title, news_p
 
 # ## JPL Space Images Featured Image
+def featured_image(browser):
+    # Visit URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
 
-# Visit URL
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
+    # Find the element by the tag 'button', and we want the second button which the the "FULL IMAGE" button
+    full_image_elem = browser.find_by_tag('button')[1]
+    # Click the button
+    full_image_elem.click()
 
-# Find the element by the tag 'button', and we want the second button which the the "FULL IMAGE" button
-full_image_elem = browser.find_by_tag('button')[1]
-# Click the button
-full_image_elem.click()
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
 
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    # Add try/except for error handling
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+    
+    except AttributeError:
+        return None
+    
+    # Use the base URL to create an absolute URL
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 
-# Find the relative image url
-img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-img_url_rel
-
-# Use the base URL to create an absolute URL
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
+    return img_url
 
 # ## Mars Facts
+def mars_facts():
+    # Add try/except for error handling
+    try:
+        # Use 'read_html' to scrap the facts table into a DF
+        # read_html() specifically search for and returns a list of 'tables' found in the HTML
+        # with index of 0 --> pull only the first table found
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
 
-# Scrape the entire table using PANDAS .read_html() function
-# read_html() specifically search for and returns a list of 'tables' found in the HTML
-# with index of 0 --> pull only the first table found
-df = pd.read_html('https://galaxyfacts-mars.com')[0]
-df.head()
-df.columns=['description', 'Mars', 'Earth']
-df.set_index('description', inplace=True)
-df
+    except BaseException:
+        return None
+    
+    # Assign columns and set index of dataframe
+    df.columns=['description', 'Mars', 'Earth']
+    df.set_index('description', inplace=True)
 
-# Convert DF back into HTML-ready code
-df.to_html()
+    # Convert DF back into HTML format, add bootstrap
+    return df.to_html()
 
 # End the automated browsing session
 browser.quit()
