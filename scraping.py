5d13bce2ea7614808b52ebf43f040166a38697f3
Kobert Lam <kobertlam@gmail.com>
2021-12-27 14:18:43 -0500

    completed section 10.5.3

--- scraping.py
+++ scraping.py
@@ -1,16 +1,40 @@
 # Import dependencies
 import pandas as pd
+import datetime as dt
 
 # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Set the executable path and initialize the Chrome browser in Splinter
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+def scrape_all():
+    # 1) Initialize the browser
+    # 2) Create a data dictionary
+    # 3) End the WebDriver and return the scraped data
 
-def mar_news(browser):
+    # Set the executable path and 
+    # initialize the headless webdriver (Chrome browser in Splinter)
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    # Set the news title and paragraph variaibles
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    # Stop webdriver (end the automated browsing session) and return data
+    browser.quit()
+    return data
+
+
+def mars_news(browser):
     # Visit the mars nasa news site
     url = 'https://redplanetscience.com'
     browser.visit(url)
@@ -32,7 +56,7 @@ def mar_news(browser):
 
         # Use the parent element to find the paragraph text
         news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-
+    
     except AttributeError:
         return None, None
     # Return the news title and paragraph
@@ -85,9 +109,6 @@ def mars_facts():
     # Convert DF back into HTML format, add bootstrap
     return df.to_html()
 
-# End the automated browsing session
-browser.quit()
-
-
-
-
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
